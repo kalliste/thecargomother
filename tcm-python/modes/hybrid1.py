41fe25214085732f2790700bde6cf94f25fa6288
Joshua Jackson <jjackson@kallisteconsulting.com>
2013-08-26 22:36:30 -0500

    more text

--- tcm-python/modes/hybrid1.py
+++ tcm-python/modes/hybrid1.py
@@ -7,7 +7,8 @@ def init():
   global vplayer
   global videos
   global phrases
-  phrases = load_text_set("burn1")
+  phrases = load_text_set("burn1") + load_text_set("cursing") + load_text_set("daniel") + load_text_set("jewelz")
+  random.shuffle(phrases)
   vplayer = Player(('-fs -fixed-vo'))
   videos = load_video_list("shortsilent")
   play_random_vid()
@@ -27,6 +28,8 @@ def deinit():
 
 def event(num):
   global phrases
+  if (random.randint(0, 25) == 23):
+    go_random_mode()
   phrase = phrases[num % len(phrases)]
   say_phrase(phrase)
 
@@ -35,3 +38,4 @@ def tick():
   x = vplayer.stream_pos
   if (vplayer.stream_end == None):
     play_random_vid()
+
